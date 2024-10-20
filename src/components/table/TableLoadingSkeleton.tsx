9283c9a02e49422cf7a5b4a35e7a97af65fc3a73
rasty <hvozdetskiyyrii@gmail.com>
2024-10-20 16:03:40 +0300

    update skeleton

--- src/components/table/TableLoadingSkeleton.tsx
+++ src/components/table/TableLoadingSkeleton.tsx
@@ -4,10 +4,11 @@ function TableLoadingSkeleton() {
 
    return (
       <div className="space-y-2">
-         {[...Array(5)].map((_, i) => (
+         {[...Array(8)].map((_, i) => (
             <div key={i} className="flex items-center space-x-4">
-               <Skeleton className="h-4 w-[300px]"/>
-               <Skeleton className="h-4 flex-1"/>
+               <Skeleton className="h-10 w-[300px]"/>
+               <Skeleton className="h-10 flex-1"/>
+               <Skeleton className="h-10 flex-1"/>
             </div>
          ))}
       </div>
